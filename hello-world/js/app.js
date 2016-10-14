3fcec422b0831f951deb16c6f8b971621743b988
Matheus Pessoa <matheusmpessoa17@gmail.com>
2016-10-14 09:16:09 -0300

    bower e package inseridos. Flexbox criada. CDN bootstrap inserida

---
+++ hello-world/js/app.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDAg5csvj-YyL7MsP3sn6uUrwcwMn6TwLw",
+    authDomain: "fir-web-starter-913b8.firebaseapp.com",
+    databaseURL: "https://fir-web-starter-913b8.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "184098779639"
+};
+firebase.initializeApp(config);
